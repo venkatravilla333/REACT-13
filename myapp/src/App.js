506a3c9937b9d8cdd062c3e5942c3586427ee17f
venkat ravilla <venkatravilla333@gmail.com>
2025-01-21 13:24:59 +0530

    JSX

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,13 +2,16 @@
 import Child from './components/functional-com/Child'
 // import Parent  from './components/functional-com/Parent'
 import Parent  from './components/class-com/Parent'
+import Jsx from './components/functional-com/JSX/Jsx'
 
 function App() {
   return (<div>
     <h2>App component</h2>
     {/* <Parent /> */}
     {new Parent().render()}
-    
+
+    {/* {Jsx()} */}
+    <Jsx/>
     </div>)
 }
 
