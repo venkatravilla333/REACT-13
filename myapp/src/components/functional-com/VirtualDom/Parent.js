c107a381a5295a369c3c331139d9ed36974d3f44
venkat ravilla <venkatravilla333@gmail.com>
2025-02-22 14:26:39 +0530

    virtual dom

--- myapp/src/components/functional-com/VirtualDom/Parent.js
+++ myapp/src/components/functional-com/VirtualDom/Parent.js
@@ -5,7 +5,8 @@ function Parent() {
   var [count, setCount] = useState(0)
   console.log('parent render')
   // var x = 100 //primitive
-  var x = {value: 100} //reference
+  var x = { value: 100 } //reference
+  
   return (
     <div>
       <h4>Parent: {count}</h4>
